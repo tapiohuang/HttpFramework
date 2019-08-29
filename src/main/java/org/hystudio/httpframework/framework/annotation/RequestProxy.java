00a253559faf895bb6242436493030ce8c82217a
tapiohuang <46253261+tapiohuang@users.noreply.github.com>
2019-08-29 09:27:04 +0800

    Signed-off-by: tapiohuang <46253261+tapiohuang@users.noreply.github.com>

--- src/main/java/org/hystudio/httpframework/framework/annotation/RequestProxy.java
+++ src/main/java/org/hystudio/httpframework/framework/annotation/RequestProxy.java
@@ -2,6 +2,9 @@ package org.hystudio.httpframework.framework.annotation;
 
 import java.lang.annotation.*;
 
+/**
+ * Proxy参数注解
+ */
 @Target({ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
