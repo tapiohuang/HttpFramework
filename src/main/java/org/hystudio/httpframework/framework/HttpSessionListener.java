20dfdd2584cd7df1036d826615af43bd618108e2
tapiohuang <46253261+tapiohuang@users.noreply.github.com>
2019-09-04 16:44:19 +0800

    Signed-off-by: tapiohuang <46253261+tapiohuang@users.noreply.github.com>

--- src/main/java/org/hystudio/httpframework/framework/HttpSessionListener.java
+++ src/main/java/org/hystudio/httpframework/framework/HttpSessionListener.java
@@ -3,10 +3,19 @@ package org.hystudio.httpframework.framework;
 public class HttpSessionListener {
     private static HttpSessionListener ourInstance = new HttpSessionListener();
 
+    private HttpSessionListener() {
+    }
+
     public static HttpSessionListener getInstance() {
         return ourInstance;
     }
 
-    private HttpSessionListener() {
+    public void sendEvent(String msg) {
+        System.out.println(msg);
+    }
+
+    public void sendEvent(Throwable throwable) {
+        System.out.println("执行监听器");
+        throwable.printStackTrace();
     }
 }
