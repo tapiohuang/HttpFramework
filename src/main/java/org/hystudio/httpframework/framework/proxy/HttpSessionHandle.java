9e05b9f6ddb6be6f5b2a984079651222c0737c2c
tapiohuang <46253261+tapiohuang@users.noreply.github.com>
2019-09-05 22:23:07 +0800

    Signed-off-by: tapiohuang <46253261+tapiohuang@users.noreply.github.com>

--- src/main/java/org/hystudio/httpframework/framework/proxy/HttpSessionHandle.java
+++ src/main/java/org/hystudio/httpframework/framework/proxy/HttpSessionHandle.java
@@ -1,24 +1,43 @@
 package org.hystudio.httpframework.framework.proxy;
 
 import org.hystudio.httpframework.framework.*;
-import org.hystudio.httpframework.framework.handle.RequestHandle;
 import org.springframework.cglib.proxy.MethodInterceptor;
 import org.springframework.cglib.proxy.MethodProxy;
+import org.springframework.context.ApplicationContext;
 
 import java.lang.reflect.Method;
 
 public class HttpSessionHandle implements MethodInterceptor {
     private HttpSessionThreadPool httpSessionThreadPool;
-
+    private ApplicationContext applicationContext;
     private HttpSessionFactory httpSessionFactory;
+    private boolean inited = false;
 
-    public HttpSessionHandle(HttpSessionThreadPool httpSessionThreadPool, HttpSessionFactory httpSessionFactory) {
-        this.httpSessionFactory = httpSessionFactory;
-        this.httpSessionThreadPool = httpSessionThreadPool;
+    public HttpSessionHandle(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    private synchronized void init() {
+        if (httpSessionThreadPool == null) {
+            httpSessionThreadPool = applicationContext.getBean(HttpSessionThreadPool.class);
+        }
+        if (httpSessionFactory == null) {
+            httpSessionFactory = applicationContext.getBean(HttpSessionFactory.class);
+        }
+        if (httpSessionThreadPool == null) {
+            throw new RuntimeException("httpSessionThreadPool is null");
+        }
+        if (httpSessionFactory == null) {
+            throw new RuntimeException("httpSessionFactory is null");
+        }
+        inited = true;
     }
 
     @Override
     public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        if (!inited) {
+            init();
+        }
         HttpSession httpSession = httpSessionFactory.createHttpSession(method, objects);
         httpSessionThreadPool.addSessionExecutor(httpSession);
         return httpSession.readResult();
