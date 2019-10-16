d285ffd9633989f49d5e98c4fe15e8b55b1dfa77
tapiohuang <46253261+tapiohuang@users.noreply.github.com>
2019-10-16 20:55:23 +0800

    Signed-off-by: tapiohuang <46253261+tapiohuang@users.noreply.github.com>

--- src/main/java/org/hystudio/httpframework/framework2/reader/AbstractReader.java
+++ src/main/java/org/hystudio/httpframework/framework2/reader/AbstractReader.java
@@ -1,8 +1,15 @@
 package org.hystudio.httpframework.framework2.reader;
 
-public class AbstractReader implements IReader {
-    @Override
-    public void read() {
+import org.hystudio.httpframework.framework2.session.HttpSession;
 
+public abstract class AbstractReader implements IReader {
+    private HttpSession httpSession;
+
+    protected AbstractReader(HttpSession httpSession) {
+        this.httpSession = httpSession;
     }
+
+
+    @Override
+    public abstract void read();
 }
